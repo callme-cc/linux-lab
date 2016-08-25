49fb0991ca694cd26898d4fc2d64f5b9dda1501b
Wu Zhangjin <wuzhangjin@gmail.com>
2016-08-26 00:10:59 +0800

    tools: Allow to generate dynamic vnc ports
    This allows to launch different containers for different users/students.
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/run-docker-lab-daemon.sh
+++ tools/run-docker-lab-daemon.sh
@@ -3,13 +3,16 @@
 # start the linux lab via docker with the shared source in $PWD/
 #
 
+TOP_DIR=$(dirname `readlink -f $0`)
+
 docker_image=tinylab/linux-lab
-local_lab_dir=$PWD/
+local_lab_dir=`dirname $TOP_DIR`
 remote_lab_dir=/linux-lab/
 
 browser=chromium-browser
-port=6080
-url=http://localhost:$port/vnc.html
+remote_port=6080
+local_port=$((RANDOM/500+6080))
+url=http://localhost:$local_port/vnc.html
 pwd=ubuntu
 
 # nfsd.ko must be inserted to enable nfs kernel server
@@ -18,14 +21,15 @@ sudo modprobe nfsd
 CONTAINER_ID=$(docker run --privileged \
                 --cap-add sys_admin --cap-add net_admin \
                 --device=/dev/net/tun \
-                -d -p $port:$port \
+                -d -p $local_port:$remote_port \
                 -v $local_lab_dir:$remote_lab_dir \
                 $docker_image)
 
 docker logs $CONTAINER_ID | sed -n 1p
 
+echo $local_port > $TOP_DIR/lab_local_port
 which $browser 2>&1>/dev/null \
     && ($browser $url 2>&1>/dev/null &) \
-    && echo "please login with password: $pwd" && exit 0
+    && echo "please login $url with password: $pwd" && exit 0
 
 echo "Usage: Please open $url with password: $pwd"
